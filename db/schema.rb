d89976d91c7e4a6232da48a9ce3622ef4f26821b
Steven Dempsey <stevendempsey13@gmail.com>
2022-01-21 12:49:33 -0800

    Setup team and player relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_195930) do
+ActiveRecord::Schema.define(version: 2022_01_21_201747) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "team_id", null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2022_01_21_195930) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "players", "teams"
 end
