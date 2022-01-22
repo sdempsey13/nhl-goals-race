0a2ddcdaabef642cd889a40b2c8457dd87a0fd5c
Steven Dempsey <stevendempsey13@gmail.com>
2022-01-22 15:27:15 -0800

    Add team validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,8 @@
 class Team < ApplicationRecord
     has_many :players
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :nhlID, presence: true
+    validates :name, uniqueness: true
 end
