7c018ce84715fa17ee7e989da91bbf17ad96e8ab
sdempsey13 <stevendempsey13@gmail.com>
2022-02-06 14:59:42 -0800

    Setup data.json pipe to front end

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   def index
-    data = File.open('app/data/data.json').to_json
+    data = File.open('app/data/data.json')
+
+    file_data = data.read
 
     respond_to do |format|
       format.html
-      format.json { render json: data }
+      format.json { render json: file_data }
     end
   end
 end
--- app/javascript/packs/goals_bar_chart_race.js
+++ app/javascript/packs/goals_bar_chart_race.js
@@ -1,5 +1,31 @@
 import * as d3 from "d3"
 
-$( document ).ready(function() {
-  console.log( "ready!" );
+// ajax call to fetch json
+var loadData = function(){
+  $.ajax({
+    type: 'GET',
+    contentType: 'application/json; charset=utf-8',
+    url: '/',
+    dataType: 'json',
+    success: function(data){
+      drawBarPlot(data);
+    },
+    failure: function(result){
+      error();
+    }
+  });
+};
+
+function error() {
+console.log("Something went wrong!");
+}
+
+// draw bar plot
+function drawBarPlot(data){
+  console.log(d3.min(data));
+};
+
+// fetch data on page load
+$(document).ready(function(){ 
+  loadData(); 
 });
