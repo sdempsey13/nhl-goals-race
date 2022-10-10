c5fcac1a6a4b10c7be6cef6c954797085a47006e
Steven Dempsey <stevendempsey13@gmail.com>
2022-10-10 11:37:02 -0700

    Sample data added

---
+++ app/data/sample_data.js
@@ -0,0 +1,37 @@
+[
+    [
+        {date: '2021-10-12', name: 'Max Pacioretty', team: 'Vegas Golden Knights', goals: 2}, 
+        {date: '2021-10-12', name: 'Teddy Blueger', team: 'Pittsburgh Penguins', goals: 1},
+        {date: '2021-10-12', name: 'Brian Boyle', team: 'Pittsburgh Penguins', goals: 1},
+        {date: '2021-10-12', name: 'Bryan Rust', team: 'Pittsburgh Penguins', goals: 1},
+        {date: '2021-10-12', name: 'Danton Heinen', team: 'Pittsburgh Penguins', goals: 1}
+    ],
+    [
+        {date: '2021-10-13', name: 'Alex Ovechkin', team: 'Washington Capitals', goals: 2},
+        {date: '2021-10-13', name: 'Max Pacioretty', team: 'Vegas Golden Knights', goals: 2},
+        {date: '2021-10-13', name: 'Chris Kreider', team: 'New York Rangers', goals: 1},
+        {date: '2021-10-13', name: 'Teddy Blueger', team: 'Pittsburgh Penguins', goals: 1},
+        {date: '2021-10-13', name: 'Brian Boyle', team: 'Pittsburgh Penguins', goals: 1}
+    ],
+    [
+        {date: '2021-10-14', name: 'Tyler Bertuzzi', team: 'Detroit Red Wings', goals: 4},
+        {date: '2021-10-14', name: 'Anze Kopitar', team: 'Los Angeles Kings', goals: 3},
+        {date: '2021-10-14', name: 'Chris Kreider', team: 'New York Rangers', goals: 2},
+        {date: '2021-10-14', name: 'Danton Heinen', team: 'Pittsburgh Penguins', goals: 2},
+        {date: '2021-10-14', name: 'Evan Rodrigues', team: 'Pittsburgh Penguins', goals: 2}
+    ],
+    [
+        {date: '2021-10-15', name: 'Tyler Bertuzzi', team: 'Detroit Red Wings', goals: 4},
+        {date: '2021-10-15', name: 'Anze Kopitar', team: 'Los Angeles Kings', goals: 3},
+        {date: '2021-10-15', name: 'Jack Hughes', team: 'New Jersey Devils', goals: 2},
+        {date: '2021-10-15', name: 'Danton Heinen', team: 'Pittsburgh Penguins', goals: 2},
+        {date: '2021-10-15', name: 'Evan Rodrigues', team: 'Pittsburgh Penguins', goals: 2}
+    ],
+    [
+        {date: '2021-10-16', name: 'Tyler Bertuzzi', team: 'Detroit Red Wings', goals: 4},
+        {date: '2021-10-16', name: 'Anze Kopitar', team: 'Los Angeles Kings', goals: 4},
+        {date: '2021-10-16', name: 'Chris Kreider', team: 'New York Rangers', goals: 3},
+        {date: '2021-10-16', name: 'Danton Heinen', team: 'Pittsburgh Penguins', goals: 3},
+        {date: '2021-10-16', name: 'Andrei Svechnikov', team: 'Carolina Hurricanes', goals: 3}
+    ]
+]
